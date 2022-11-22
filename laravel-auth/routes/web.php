32dadeabcbcfee98dd901d5170e6f4392e11abcc
ErosEros <eros.vittori1994@gmail.com>
2022-11-22 19:46:08 +0100

    add route for guest.home

--- laravel-auth/routes/web.php
+++ laravel-auth/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
@@ -30,3 +30,8 @@ Route::middleware('auth')
     });
 
     Auth::routes();
+
+
+    Route::get('{any?}', function() {
+        return view("guest.home");
+    })->where("any", ".*");
